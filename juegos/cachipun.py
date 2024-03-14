6b3f17cc124b4ef37b388822f7e6cfee6ff2a675
vicentecruzatr <vcruzatr@uc.cl>
2024-03-14 12:59:49 -0300

    kachipum final

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -4,12 +4,17 @@ def cachipun():
     jugada_1 = input("cual es tu movimiento? ")
     indice = random.randint(0,2)
     jugada_pc = posibilidades[indice]
+    print("Computador escogió", jugada_pc)
     if jugada_1 == "piedra":
+        
         if jugada_pc == "papel":
+            
             print("PERDISTE")
         if jugada_pc == "tijera":
+            
             print("GANASTE")
         if jugada_pc == "piedra":
+            
             print("EMPATE")
         
     elif jugada_1 == "papel":
